79eda4fa6b7f3542a50644de06ae3260b29792c9
shendoalex <shendo.alex@gmail.com>
2023-03-12 09:39:12 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 187;
+        short womanHeight = 155;
+        double man = Fit.manWeight(manHeight);
+        double woman = Fit.womanWeight(womanHeight);
+        System.out.println("Man 187 is " + man);
+        System.out.println("Woman 155 is" + woman);
+    }
+}
