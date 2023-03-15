5a9a71eb7b43c87e86548c381d0f709fe5ee865a
shendoalex <shendo.alex@gmail.com>
2023-03-15 21:00:09 +0300

    5.6. Простые числа [#156314]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = finish; number > 1; number--) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PrimeNumber.calc(5));
+    }
+}
