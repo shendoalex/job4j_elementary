6fe963bb9d9e28254268422e315f39446b86460c
shendoalex <shendo.alex@gmail.com>
2023-03-17 13:00:36 +0300

    6.3. Массив заполнен true или false [#53857]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        /*for () {
+            int min = MinDiapason.findMin(data,start,data.length -1);
+            int index = FindLoop.indexInRange(data, min, start, data.length -1);
+            */
+
+            /* swap(...)
+        } */
+        return data;
+    }
+}
