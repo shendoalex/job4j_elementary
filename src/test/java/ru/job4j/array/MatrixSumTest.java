d85526fce38054203d7ebd5f0f0fcedb41f7d75d
shendoalex <shendo.alex@gmail.com>
2023-03-18 19:38:51 +0300

    MatrixSum fix

--- src/test/java/ru/job4j/array/MatrixSumTest.java
+++ src/test/java/ru/job4j/array/MatrixSumTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MatrixSumTest {
 
     @Test
-    public void WhenSingle() {
+    public void whenSingle() {
         int[][] array = {{10}};
         int result = MatrixSum.sum(array);
         int expected = 10;
@@ -14,7 +14,7 @@ public class MatrixSumTest {
     }
 
     @Test
-    public void WhenTwo() {
+    public void whenTwo() {
         int[][] array = {{1, 2}, {1, 2}};
         int result = MatrixSum.sum(array);
         int expected = 6;
@@ -22,7 +22,7 @@ public class MatrixSumTest {
     }
 
     @Test
-    public void WhenThree() {
+    public void whenThree() {
         int[][] array = {{1, 2, 1}, {1, 2, 0}};
         int result = MatrixSum.sum(array);
         int expected = 7;
