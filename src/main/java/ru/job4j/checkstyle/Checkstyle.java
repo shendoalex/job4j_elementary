0efb05b8e8dcc71f7e3478f44970020620d001d5
shendoalex <shendo.alex@gmail.com>
2023-03-23 08:25:31 +0300

    Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Checkstyle.java
+++ src/main/java/ru/job4j/checkstyle/Checkstyle.java
@@ -3,21 +3,21 @@ package ru.job4j.checkstyle;
 public class Checkstyle {
     public static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surname;
+    private String surname;
+    private String name;
 
-    void echo() {
+    public Checkstyle() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
-        String name;
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
-    }
-
-    void broken() {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
